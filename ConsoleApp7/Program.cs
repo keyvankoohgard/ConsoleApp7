f840f74436eb438bde8d64ba90d3fa80a8f84cde
keyvan <keyvankoohgard61@gmail.com>
2022-11-20 10:57:45 +0330

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,26 @@
+﻿//BAnk accountint value //
+int value = Convert.ToInt32(Console.ReadLine());
+int benefit = Convert.ToInt32(Console.ReadLine());
+int target = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(calc(value, benefit, target));
+
+int calc(int value, int benefit, int target)
+{
+    float temp = value;
+    int years = 0;
+
+    while (true)
+    {
+        if (temp >= target)
+        {
+            break;
+        }
+        else
+        {
+            temp += (temp * benefit) / 100;
+            years++;
+        }
+    }
+    return years;
+}
